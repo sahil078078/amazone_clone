c8058bf49f786ebbbbfa8b35ed5d2db056332f04
Sahil <aorking.shp007@gmail.com>
2022-06-23 18:00:17 +0530

    making custom button and customTextFrom field

--- lib/features/auth/auth_screen.dart
+++ lib/features/auth/auth_screen.dart
@@ -1,6 +1,7 @@
-import 'dart:developer';
+import 'package:amazone_clone/commons/widgets/custom_button.dart';
 import 'package:amazone_clone/commons/widgets/custom_textfield.dart';
 import 'package:amazone_clone/constants/global_variable.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -22,6 +23,22 @@ class _AuthScreenState extends State<AuthScreen> {
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _mobileNumber = TextEditingController();
+
+  // delete / dispose all controller for allocate space
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    _mobileNumber.dispose();
+    super.dispose();
+  }
+
+  bool obscureTextPassword = true;
+  IconData? icon;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,67 +46,197 @@ class _AuthScreenState extends State<AuthScreen> {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
-          child: Column(
-            children: [
-              Text(
-                'Welcome',
-                style: GoogleFonts.nunito(
-                  fontSize: 22,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-              ListTile(
-                contentPadding: EdgeInsets.zero,
-                title: Text(
-                  'Create Account',
-                  style: GoogleFonts.nunito(
-                    fontSize: 18,
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            physics: const BouncingScrollPhysics(),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Welcome',
+                  style: GoogleFonts.poppins(
+                    fontSize: 23,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black.withOpacity(0.62),
                   ),
                 ),
-                leading: Radio(
-                  activeColor: GlobalVariable.secondaryColor,
-                  onChanged: (Auth? val) {
-                    setState(() {
-                      _auth = val!;
-                    });
-                    log('$val');
-                  },
-                  value: Auth.signup,
-                  groupValue: _auth,
-                ),
-              ),
-              if (_auth == Auth.signup)
-                Form(
-                  key: _signUpFormKey,
-                  child: Column(
-                    children: [
-                      CustomTextField(controller: _nameController),
-                      CustomTextField(controller: _emailController),
-                      CustomTextField(controller: _passwordController),
-                    ],
+                ListTile(
+                  contentPadding: EdgeInsets.zero,
+                  title: Text(
+                    'Create Account',
+                    style: GoogleFonts.nunito(
+                      fontSize: 18,
+                    ),
+                  ),
+                  leading: Radio(
+                    activeColor: GlobalVariable.secondaryColor.withOpacity(0.7),
+                    onChanged: (Auth? val) {
+                      setState(() {
+                        _auth = val!;
+                      });
+                      // log('$val');
+                    },
+                    value: Auth.signup,
+                    groupValue: _auth,
                   ),
                 ),
-              ListTile(
-                contentPadding: EdgeInsets.zero,
-                title: Text(
-                  'Sign In',
-                  style: GoogleFonts.nunito(
-                    fontSize: 18,
+                if (_auth == Auth.signup)
+                  Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Colors.black.withOpacity(0.0235),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    padding: const EdgeInsets.only(
+                      bottom: 10,
+                      top: 10,
+                      left: 8,
+                      right: 8,
+                    ),
+                    child: Form(
+                      key: _signUpFormKey,
+                      child: SingleChildScrollView(
+                        physics: const BouncingScrollPhysics(),
+                        scrollDirection: Axis.vertical,
+                        child: Column(
+                          children: [
+                            CustomTextField(
+                              controller: _nameController,
+                              hindText: 'Enter user name',
+                              lableText: 'Name',
+                              prefixIcon: const Icon(
+                                CupertinoIcons.person,
+                                color: GlobalVariable.secondaryColor,
+                              ),
+                              maxLength: 20,
+
+                              // here below inputFormatters user will input only capital latters and small latters only
+
+                              // inputFormatters: [
+                              //   FilteringTextInputFormatter.allow(
+                              //     RegExp("[a-z A-Z]"),
+                              //   ),
+                              // ],
+
+                              keyboardType: TextInputType.text,
+                              validator: (value) {
+                                Pattern pattern =
+                                    (r'[!@#<>?":_`~;[\]\\|=+)(*&^%0-9-]');
+                                RegExp regExp = RegExp(pattern.toString());
+                                if (value == null || value.isEmpty) {
+                                  return 'Please enter name';
+                                } else if (value.trim().contains(regExp)) {
+                                  return 'Please enter valid name ';
+                                } else {
+                                  return null;
+                                }
+                              },
+                            ),
+                            CustomTextField(
+                              controller: _mobileNumber,
+                              lableText: 'Mobile',
+                              hindText: 'Enter mobile number',
+                              prefixIcon: const Icon(
+                                Icons.phone_android,
+                                color: GlobalVariable.secondaryColor,
+                              ),
+                            ),
+                            CustomTextField(
+                              controller: _emailController,
+                              hindText: 'Enter email',
+                              lableText: 'Email',
+                              prefixIcon: const Icon(
+                                CupertinoIcons.mail,
+                                color: GlobalVariable.secondaryColor,
+                              ),
+                            ),
+                            CustomTextField(
+                              controller: _passwordController,
+                              hindText: 'Enter password',
+                              lableText: 'Password',
+                              obscureText: obscureTextPassword,
+                              prefixIcon: const Icon(
+                                CupertinoIcons.lock_shield,
+                                color: GlobalVariable.secondaryColor,
+                              ),
+                              suffixIcon: GestureDetector(
+                                onTap: () => setState(() {
+                                  obscureTextPassword = !obscureTextPassword;
+                                }),
+                                child: obscureTextPassword
+                                    ? Icon(
+                                        CupertinoIcons.eye,
+                                        color: GlobalVariable.secondaryColor
+                                            .withOpacity(0.5),
+                                      )
+                                    : Icon(
+                                        CupertinoIcons.eye_slash,
+                                        color: GlobalVariable.secondaryColor
+                                            .withOpacity(0.7),
+                                      ),
+                              ),
+                            ),
+                            CustomButtom(
+                              buttonText: 'Sign Up',
+                              buttonColor: GlobalVariable.secondaryColor
+                                  .withOpacity(0.8),
+                              onPressed: () {
+                                if (_signUpFormKey.currentState!.validate()) {
+                                  _signUpFormKey.currentState!.save();
+                                }
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ListTile(
+                  contentPadding: EdgeInsets.zero,
+                  title: Text(
+                    'Sign In',
+                    style: GoogleFonts.nunito(
+                      fontSize: 18,
+                    ),
+                  ),
+                  leading: Radio(
+                    activeColor: GlobalVariable.secondaryColor.withOpacity(0.7),
+                    onChanged: (Auth? val) {
+                      setState(() {
+                        _auth = val!;
+                      });
+                      // log('$val');
+                    },
+                    value: Auth.signin,
+                    groupValue: _auth,
                   ),
                 ),
-                leading: Radio(
-                  activeColor: GlobalVariable.secondaryColor,
-                  onChanged: (Auth? val) {
-                    setState(() {
-                      _auth = val!;
-                    });
-                    log('$val');
-                  },
-                  value: Auth.signin,
-                  groupValue: _auth,
-                ),
-              ),
-            ],
+                if (_auth == Auth.signin)
+                  Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Colors.black.withOpacity(0.0235),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    padding: const EdgeInsets.only(
+                      bottom: 10,
+                      top: 10,
+                      left: 8,
+                      right: 8,
+                    ),
+                    child: Form(
+                      child: SingleChildScrollView(
+                        physics: const BouncingScrollPhysics(),
+                        child: Column(
+                          children: const [
+                            Text('bhuro'),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+              ],
+            ),
           ),
         ),
       ),
